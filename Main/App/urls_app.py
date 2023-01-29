3f668c25a3a8e9c47e88585069ceb9b34dab408b
Aiwoen <52808743+Aiwoen@users.noreply.github.com>
2023-01-29 17:24:10 +0800

    11201291724 備份

--- Main/App/urls_app.py
+++ Main/App/urls_app.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include,re_path
 from App import views
 
 urlpatterns=[
@@ -10,5 +10,9 @@ urlpatterns=[
     path('test',views.test),
     path('addpersons',views.add_persons),
     path('getperson',views.get_persons),
-    path('classlist',views.classlist),
+    path('classlist/',views.classlist,name='classlist'),
+    path('classlists/<int:id>/',views.classstudentlist,name='class_students'),
+    path('addcstudent/<int:class_id>',views.class_student_add,name='class_student_add'),
+    path('deletecstudent/<int:id>/<int:class_id>/',views.class_student_delete,name='class_student_delete'),
+    path('newclass',views.newclass,name='newclass'),
 ]
--- Main/App/views.py
+++ Main/App/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from App.models import Student
 from App.models import Person
@@ -78,6 +78,51 @@ def get_persons(request):
 def classlist(request):
     classes = Sclass.objects.all().order_by("c_name")
     context = {
-        "classes" : classes
+        "classes" : classes,
+        "ip" : request.META.get("REMOTE_ADDR")
     }
+    remoteip(request)
     return render(request,'class.html',context=context)
+
+def classstudentlist(request,id):
+    class_students = Student.objects.filter(s_class_id=id)
+    class_id=class_students.values()[0]['s_class_id']
+    return render(request,'student_list.html',context=locals())
+
+def class_student_add(request,class_id):
+
+
+    student = Student()
+    if request.GET["n"] != '' and request.GET["a"] != '':
+        student.s_class_id = class_id
+        student.s_name = request.GET['n']
+        student.s_sage = request.GET['a']
+        student.save()
+    else:
+        print("ERROR")
+
+
+    class_students = Student.objects.filter(s_class_id=class_id)
+    return HttpResponseRedirect('/app/classlists/' + str(class_id))
+
+def class_student_delete(request,id,class_id):
+    print(id)
+
+    student = Student.objects.get(pk=id)
+    student.delete()
+
+    class_students = Student.objects.filter(s_class_id=class_id)
+    return HttpResponseRedirect('/app/classlists/' + str(class_id))
+
+def newclass(request):
+
+    #newclass = Sclass()
+    #newclass.c_name = request.GET['cn']
+    #newclass.save()
+
+    print(request.GET.get('cn'))
+
+    return render(request,'class.html',context=locals())
+def remoteip(request):
+    print("Remote IP", request.META.get("REMOTE_ADDR"))
+    return None
